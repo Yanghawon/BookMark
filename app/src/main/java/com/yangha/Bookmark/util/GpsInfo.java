88fea81fc8cd9faab0cae03da965726bdc545ef9
Yangha <drop1309@naver.com>
2017-02-26 00:41:05 +0900

    gps OK

--- app/src/main/java/com/yangha/Bookmark/util/GpsInfo.java
+++ app/src/main/java/com/yangha/Bookmark/util/GpsInfo.java
@@ -2,29 +2,20 @@ package com.yangha.Bookmark.util;
 
 import android.Manifest;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.app.ActivityCompat;
 
-/**
- * Created by Yangha on 2017-02-25.
- */
+import com.yangha.Bookmark.Activity.BaseActivity;
+import com.yangha.Bookmark.Activity.MainActivity;
 
-public class GpsInfo extends Service implements LocationListener {
-    private final Context mContext;
-
-    // 현재 GPS 사용유무
-    boolean isGPSEnabled = false;
-
-    // 네트워크 사용유무
-    boolean isNetworkEnabled = false;
-
+public class GpsInfo extends Service {
+    private final BaseActivity mContext;
+    private LocationListener mListner;
     // GPS 상태값
     boolean isGetLocation = false;
 
@@ -36,12 +27,15 @@ public class GpsInfo extends Service implements LocationListener {
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
 
     // 최소 GPS 정보 업데이트 시간 밀리세컨이므로 1분
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 5 * 1;
 
     protected LocationManager locationManager;
 
-    public GpsInfo(Context context) {
+    public GpsInfo(BaseActivity context) {
         this.mContext = context;
+        if (context instanceof MainActivity){
+            this.mListner = (MainActivity)context;
+        }
         getLocation();
     }
 
@@ -49,54 +43,34 @@ public class GpsInfo extends Service implements LocationListener {
         try {
             locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
 
-            // GPS 정보 가져오기
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                isGPSEnabled = true;
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            } else
+                locationManager.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, mListner);
+
+            if (locationManager != null) {
+                location = locationManager
+                        .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                if (location != null) {
+                    // 위도 경도 저장
+                    lat = location.getLatitude();
+                    lon = location.getLongitude();
+                }
             }
 
-            // 현재 네트워크 상태 값 알아오기
-            isNetworkEnabled = locationManager
-                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-            if (!isGPSEnabled && !isNetworkEnabled) {
-                // GPS 와 네트워크사용이 가능하지 않을때 소스 구현
-            } else {
-                this.isGetLocation = true;
-                // 네트워크 정보로 부터 위치값 가져오기
-
-                if (isNetworkEnabled) {
-
-                    locationManager.requestLocationUpdates(
-                            LocationManager.NETWORK_PROVIDER,
-                            MIN_TIME_BW_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-
-                    if (locationManager != null) {
-                        location = locationManager
-                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                        if (location != null) {
-                            // 위도 경도 저장
-                            lat = location.getLatitude();
-                            lon = location.getLongitude();
-                        }
-                    }
-                }
-
-                if (isGPSEnabled) {
-                    if (location == null) {
-                        locationManager.requestLocationUpdates(
-                                LocationManager.GPS_PROVIDER,
-                                MIN_TIME_BW_UPDATES,
-                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                        if (locationManager != null) {
-                            location = locationManager
-                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                            if (location != null) {
-                                lat = location.getLatitude();
-                                lon = location.getLongitude();
-                            }
-                        }
+            if (location == null) {
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, mListner);
+                if (locationManager != null) {
+                    location = locationManager
+                            .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location != null) {
+                        lat = location.getLatitude();
+                        lon = location.getLongitude();
                     }
                 }
             }
@@ -112,9 +86,8 @@ public class GpsInfo extends Service implements LocationListener {
     public void stopUsingGPS() {
         if (locationManager != null) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                isGPSEnabled = true;
-            }
-            locationManager.removeUpdates(GpsInfo.this);
+            }else
+                locationManager.removeUpdates(mListner);
         }
     }
 
@@ -151,23 +124,7 @@ public class GpsInfo extends Service implements LocationListener {
         return null;
     }
 
-    public void onLocationChanged(Location location) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void onProviderEnabled(String provider) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void onProviderDisabled(String provider) {
-        // TODO Auto-generated method stub
-
+    public void setmListner(LocationListener mListner) {
+        this.mListner = mListner;
     }
 }
